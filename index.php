5fbd76c4398d9695931f23643845234c78431f24
Rafa31993 <rafaaelsilva21@hotmail.com>
2024-11-06 00:39:11 +0000

    adicionar codigo

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jogo da velha</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>jogo da velha</h1>
+    
+
+    <div id="tabuleiro">
+        <div class="casa"></div>
+        <div class="casa"></div>
+        <div class="casa"></div>
+        <div class="casa"></div>
+        <div class="casa"></div>
+        <div class="casa"></div>
+        <div class="casa"></div>
+        <div class="casa"></div>
+        <div class="casa"></div>
+
+    </div>
+</body>
+</html>
